f44838a72d8fe70d5a8ec970245a5ae5828c33a9
Mariana Barerra <mebz1991@gmail.com>
2024-06-18 19:38:03 -0400

    SE AGREGARON LAS VARIABLES DESDE PROCESS

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: USER_DB,
-  host: HOST_DB,
-  database: NAME_DB,
-  password: PASSWORD_DB,
-  port: PORT_DB
+ user: process.env.USER_DB,
+ host: process.env.HOST_DB,
+ database: process.env.NAME_DB,
+ password: process.env.PASSWORD_DB,
+ port: process.env.PORT_DB
 });
 
 module.exports = pool;
